4bb7342a1753bd7e4ac0fd80339aeceb24448bda
GongSeason <2492596620@qq.com>
2025-01-26 15:49:25 +0800

    add program runtime

---
+++ Abstraction/Program.cs
@@ -0,0 +1,23 @@
+﻿//Using abstraction to work with different types of students
+
+using Abstraction;
+
+Student undergraduateStudent = new UndergraduateStudent()
+{
+    Name = "Sahana Bhat",
+    Age = 18,
+    Major = "Computer Science"
+};
+
+Student graduateStudent = new GraduateStudent()
+{
+    Name = "Ritu Verma",
+    Age = 20,
+    ResearchArea = "AI"
+};
+
+undergraduateStudent.WelcomeMessage();
+undergraduateStudent.DisplayInformation();
+Console.WriteLine("\n------------------\n");
+graduateStudent.WelcomeMessage();
+graduateStudent.DisplayInformation();
---
+++ Interface/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Interface;
+
+ICustomer customer = new Customer(1, "Rita", "B", "Rita.b@example.com", "123-456-789",
+    new DateTime(1985, 5, 15), "123 Main Street");
+
+Console.WriteLine(customer.GetFullName());
+
+
+Customer customer2 = new Customer(2, "Roy", "A", "Roy.b@example.com", "123-456-789",
+    new DateTime(1985, 5, 15), "123 Main Street"); 
+customer2.PrintCusomerInfo();
+
+

1528f4f097f8d4ef609004efb251c4ab286b2e38
GongSeason <2492596620@qq.com>
2025-01-26 11:32:07 +0800

    create leture1 folder

---
+++ Leture 1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class printToScreenRandomNumber
+{
+    Random myRand = new Random();
+
+    public void printToScreen()
+    {
+        Console.WriteLine(myRand.Next().ToString("N0"));
+    }
+
+    private void printToPrivate()
+    {
+        Console.WriteLine(myRand.Next().ToString("N0"));
+    }
+    protected void printToProtected()
+    {
+        Console.WriteLine(myRand.Next().ToString("N0"));
+    }
+}
+
+class printScreenV2 : printToScreenRandomNumber
+{
+    public void printProtected()
+    {
+       this.printToProtected();
+    }
+}
